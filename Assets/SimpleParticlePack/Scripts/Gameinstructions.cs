2eebd4960a28396f64a3993441be02fb4431c0b7
Hector2313 <158297297+Hector2313@users.noreply.github.com>
2024-05-20 08:45:59 -0400

    Initial commit

---
+++ Assets/SimpleParticlePack/Scripts/Gameinstructions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("use W, A, S, D to control the character. go through thw maze and collect the key!");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.log(messages);
+    }
+}
